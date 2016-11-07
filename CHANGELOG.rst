2df675801723d6567a3745f18e4ea3e52c6631c3
Lafaye Philippe <lafaye@emencia.com>
2016-11-07 18:06:51 +0100

    new version

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,7 +1,12 @@
 Changelog
 =========
 
-0.1
----
+0.1.1
+-----
 
-First version
+Use rst syntax for doc/descriptions rather markdown.
+
+0.1.0
+-----
+
+First version.
--- datadownloader/__init__.py
+++ datadownloader/__init__.py
@@ -2,4 +2,4 @@
 Django app tool that add an admin interface for manage archives of database's
 json dumps and/or media datas
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
