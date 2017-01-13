626bdfdc7e789b60a3ce62a2904766724ae3e955
Grégoire ROCHER <gr@enix.org>
2017-01-13 16:06:26 +0100

    Set an archive name

--- datadownloader/models.py
+++ datadownloader/models.py
@@ -88,7 +88,8 @@ class Dump(object):
         self._ensure_base_path()
         with tarfile.open(self.path, "w:gz") as tar:
             for folder in folders:
-                tar.add(folder)
+                archive_name = folder.replace(settings.BASE_DIR, '').lstrip('/')
+                tar.add(folder, archive_name)
 
     def destroy(self):
         os.remove(self.path)
