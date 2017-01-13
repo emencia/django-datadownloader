ed62c2e75fd75d00332d9e13aa6df60c258b199a
Grégoire ROCHER <gr@enix.org>
2017-01-13 15:41:13 +0100

    Implements dr_dump > 1

--- datadownloader/models.py
+++ datadownloader/models.py
@@ -7,7 +7,9 @@ import shutil
 import datetime
 import subprocess
 import tarfile
+import drdump
 
+from django.core.management import call_command
 from django.conf import settings
 
 
@@ -21,10 +23,11 @@ def get_base_path():
 
 
 class Dump(object):
-    def __init__(self, data_type, base_path=None):
+    def __init__(self, data_type, base_path=None, database_dumper=None):
         assert data_type in ('data', 'db', 'media')
         self.data_type = data_type
         self.base_path = base_path or get_base_path()
+        self.database_dumper = database_dumper or get_default_dumper()
 
     def get_metadata(self):
         try:
@@ -56,26 +59,9 @@ class Dump(object):
     def path(self):
         return os.path.join(self.base_path, self.archive_name)
 
-    def _get_datadump_bin(self):
-        if hasattr(settings, 'DATADUMP_BIN_PATH'):
-            return settings.DATADUMP_BIN_PATH
-        if hasattr(sys, 'real_prefix'):
-            # Running in a virtual env
-            candidate = os.path.join(sys.prefix, 'bin/datadump')
-            if os.path.exists(candidate):
-                return candidate
-
-        # Try a globally installed instance
-        return 'datadump'
-
     def _dump_media(self):
         return [settings.MEDIA_ROOT.replace("%s/" % os.getcwd(), ''), ]
 
-    def _dump_db(self):
-        self._clean_dumps_path()
-        subprocess.check_output(self._get_datadump_bin())
-        dump_path = os.path.join(settings.BASE_DIR, 'dumps')
-        return [dump_path.replace("%s/" % os.getcwd(), ''), ]
 
     def _clean_dumps_path(self):
         dumps_path = os.path.join(settings.BASE_DIR, 'dumps')
@@ -84,16 +70,14 @@ class Dump(object):
         os.mkdir(dumps_path)
 
     def create(self):
-        # be sure to be in project root folder
-        os.chdir(settings.BASE_DIR)
         folders = []
         if self.data_type == 'db':
-            folders.extend(self._dump_db())
+            folders.extend(self.database_dumper())
         elif self.data_type == 'media':
             folders.extend(self._dump_media())
         elif self.data_type == 'data':
             folders.extend(self._dump_media())
-            folders.extend(self._dump_db())
+            folders.extend(self.database_dumper())
 
         with tarfile.open(self.path, "w:gz") as tar:
             for folder in folders:
@@ -101,3 +85,44 @@ class Dump(object):
 
     def destroy(self):
         os.remove(self.path)
+
+
+class OldDrDump(object):
+    @classmethod
+    def default(cls):
+        if hasattr(settings, 'DATADUMP_BIN_PATH'):
+            return cls(settings.DATADUMP_BIN_PATH)
+        if hasattr(sys, 'real_prefix'):
+            # Running in a virtual env
+            candidate = os.path.join(sys.prefix, 'bin/datadump')
+            if os.path.exists(candidate):
+                return cls(candidate)
+
+        # Try a globally installed instance
+        return cls('datadump')
+
+    def __init__(self, bin_path):
+        self.bin_path = bin_path
+
+    def __call__(self):
+        self._clean_dumps_path()
+        subprocess.check_output(bin_path)
+        dump_path = os.path.join(settings.BASE_DIR, 'dumps')
+        return [
+            dump_path.replace("%s/" % os.getcwd(), ''),
+        ]
+
+
+class DrDump(object):
+    def __call__(self):
+        call_command('dr_dump')
+        dump_path = os.path.join(settings.BASE_DIR, 'dumps')
+        return [
+            dump_path.replace("%s/" % os.getcwd(), ''),
+        ]
+
+
+if drdump.__version__.startswith('0.'):
+    get_default_dumper = OldDrDump.default
+else:
+    get_default_dumper = DrDump
