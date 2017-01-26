8569a47ccb22a86ea37b9e99c2976d44822ae846
Grégoire ROCHER <gr@enix.org>
2017-01-26 12:30:39 +0100

    Bimp prerelease

--- datadownloader/__init__.py
+++ datadownloader/__init__.py
@@ -1,2 +1,2 @@
-"""Admin interface for manage archives of db's json dumps and/or media datas"""
-__version__ = "1.0.0b1"
+"""Admin interface to manage archives of db's json dumps and/or media datas"""
+__version__ = "1.0.0b2"
